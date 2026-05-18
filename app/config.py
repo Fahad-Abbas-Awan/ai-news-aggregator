eb9ba70f099013a0c32ba552beb8a7adcb69c608
FAHAD-ABBAS-AWAN <fahadabbaspersonal@gmail.com>
2026-05-18 18:15:50 +0500

    Deployement on render

--- app/config.py
+++ app/config.py
@@ -3,5 +3,6 @@
 # Add channel IDs for channels you want to monitor
 YOUTUBE_CHANNELS = [
 #    "UCcOQFuXnvDuIac2u2DKB7rA",  # Example channel ID
-    "UCn8ujwUInbJkBhffxqAPBVQ",
+#    "UCn8ujwUInbJkBhffxqAPBVQ", #dave
+     "UCeY0bbntWzzVIaj2z3QigXg", #NBC News
 ]
